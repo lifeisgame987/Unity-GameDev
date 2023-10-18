b68bbc189d7ac681eed4858dc7b6077d6975e913
lifeisgame987 <soumyadipbhatt987@gmail.com>
2023-10-18 18:08:35 +0530

    Prototype cardmatch modified

--- _Prototype/CardMatch/Player2.cs
+++ _Prototype/CardMatch/Player2.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
   
+  private Card _currentCard;
+  
   private Card _firstCard;
   private int _firstCardId;
   
   private Card _secondCard;
   private int _secondCardId;
-  
-  private bool _canClick = true;
   
   private void Update(){
     if(InputManager.Instance.IsMouseClicked()){
@@ -17,49 +17,46 @@ public class Player : MonoBehaviour{
       float colliderRadius = 0.2f;
       Collider2D collider = Physics2D.OverlapCircle(position, colliderRadius);
       
-      if(collider != null && _canClick){
-        if(_firstCard == null){
-          _firstCard = collider.GetComponent<Card>();
-          _firstCardId = _firstCard.CardId;
-          _firstCard.OpenCard();
-        }
-        else{
-          _secondCard = collider.GetComponent<Card>();
-          _secondCardId = _secondCard.CardId;
-          _secondCard.OpenCard();
-          
-          _canClick = false;
+      if(collider != null){
+        
+        _currentCard = collider.GetComponent<Card>();
+        
+        if(_currentCard.IsClosed()){
+          if(_firstCard == null){
+            _firstCard = _currentCard;
+            _firstCardId = _firstCard.CardId;
+            _firstCard.OpenCard();
+          }
+          else{
+            _secondCard = _currentCard;
+            _secondCardId = _secondCard.CardId;
+            _secondCard.OpenCard();
+          }
         }
       }
     }
     
-    if(_secondCard != null){
-      if(_secondCard.IsOpened()){
+    if(_firstCard != null && _secondCard != null){
+      if(_firstCard.IsOpened() && _secondCard.IsOpened()){
         if(_firstCardId == _secondCardId){
-          // Destroy card
           _firstCard.DestroySelf();
           _secondCard.DestroySelf();
-          
-          _firstCard = null;
-          _secondCard = null;
-          
-          _canClick = true;
-          
-          return;
+          RemoveCard();
         }
         else{
-          // Hide card
           _firstCard.CloseCard();
           _secondCard.CloseCard();
         }
       }
       
-      if(_secondCard.IsClosed()){
-        _firstCard = null;
-        _secondCard = null;
-        
-        _canClick = true;
+      if(_firstCard.IsClosed() && _secondCard.IsClosed()){
+        RemoveCard();
       }
     }
+  }
+  
+  private void RemoveCard(){
+    _firstCard = null;
+    _secondCard = null;
   }
 }
